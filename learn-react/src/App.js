f4bf5b2e7ccc7276d782cb28caffcc56d0eb7aa0
OtomiVetra <otomivetra@mail.ru>
2024-05-20 14:51:45 +0300

    add comments

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,9 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+// тут впринципе везде используется декларативный стиль)
 export const App = () => {
+	//хочу текущую дату, а язык уже нам ее предоставляет
 	const currentDate = new Date().toLocaleDateString();
 	return (
+		//описываем как выглядит желаемый компонент, тоже декларативный стиль
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
